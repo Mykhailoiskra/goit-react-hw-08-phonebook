f2e49027864a6f6632b9c414c8e9014964a6d96b
Mykhailo Iskra <iskramichel@gmail.com>
2021-02-03 10:02:58 +0200

    adds memoisation to selectors

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,11 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const getItems = (state) => state.contacts.items;
 export const getFilter = (state) => state.contacts.filter;
 
-export const getFilteredContacts = (state) => {
-  const contacts = getItems(state);
-  const filter = getFilter(state);
-  const normalizedFilter = filter.toLowerCase();
-  return contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-};
+export const getFilteredContacts = createSelector(
+  [getItems, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
